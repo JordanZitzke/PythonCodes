503a73d04ed7c8542bd62170236a51b850534191
JordanZitzke <jordan.zitzke.rs@gmail.com>
2023-04-14 11:17:09 -0300

    firstclass

---
+++ PythonCourse/code2.py
@@ -0,0 +1,8 @@
+#Prints com outros separadores
+#O argumento 'sep' é um separador
+
+print (10, 11, sep='-', end='##') #mudando o final, tirando ou colocando quebra de linha, e colocando ou não outros caracteres
+print (0, 1, sep='-')
+print (2, 3, sep='/')
+
+
