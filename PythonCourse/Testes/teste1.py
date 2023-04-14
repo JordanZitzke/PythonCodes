17918727e4512714b4bd50ae8fe0796074a40571
JordanZitzke <jordan.zitzke.rs@gmail.com>
2023-04-14 11:52:44 -0300

    secondclass

---
+++ PythonCourse/Testes/teste1.py
@@ -0,0 +1,3 @@
+#Teste do que será exibido 
+
+print('Explícito', 'é', 'melhor " do que implícito')
---
+++ PythonCourse/code3.py
@@ -0,0 +1,23 @@
+#Strings 
+
+#Aspas simples
+
+print ('Isso é uma string')
+
+#Aspas duplas
+
+print ("Isso também é uma string")
+
+#Abaixo temos o caractere de escape '\'
+
+print ("Jordan \" Zitzke Pinho")
+
+# Se digitar 'r' na frente o '\' é mostrado
+
+print (r"Jordan \"Zitzke\"")
+
+#Para colocar aspas em uma frase basta inverter as aspas, como abaixo
+
+print ('Jordan falou:"Saiam da frente"') #ou
+print ("Jordan disse: 'Eu vou também'")
+
