b547ec3891e93b5ddef418aefc3c4287f3ca4afb
JordanZitzke <jordan.zitzke.rs@gmail.com>
2023-09-15 14:55:00 -0300

    Update

---
+++ PythonCourse/code10.py
@@ -0,0 +1,10 @@
+#APENAS CONCATENAÇÃO COMUM
+
+conc = 'A' + 'B' + 'C'
+print(conc)
+
+a_dez_vezes = ('A' * 10)
+tres_vezes_jordan = (3 * 'Jordan')
+
+print(a_dez_vezes)
+print (tres_vezes_jordan)
---
+++ PythonCourse/code9.py
@@ -0,0 +1,25 @@
+# Operadores aritméticos
+
+adicao = 10 + 10
+subtracao = 10 - 5
+multiplicacao = 10 * 10
+divisao_normal = 10 / 2.2
+divisao_inteira = 10 // 2.2
+exponenciacao = 2 ** 10
+modulo = 55 % 2 
+
+print ('Adição', adicao)
+print ('Subtração', subtracao)
+print ('Multiplicação', multiplicacao)
+print ('Divisão', divisao_normal)
+print ('Divisão Inteira', divisao_inteira)
+print ('Exponenciação', exponenciacao)
+print ('Módulo', modulo)
+
+print ('Verificação de Restos')
+
+print ('10 % 8 ?', 10 % 8 == 0)
+print ('16 % 8 ?', 16 % 8 == 0)
+print ('10 % 2 ?', 10 % 2 == 0)
+
+
