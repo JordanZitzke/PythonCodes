8d64122401beb1aaf694e59deca7709eb4f2701e
JordanZitzke <jordan.zitzke.rs@gmail.com>
2023-09-15 15:10:44 -0300

    update 12

---
+++ PythonCourse/code12.py
@@ -0,0 +1,10 @@
+# IMC
+
+nome = 'Jordan Pinho'
+altura = 1.73
+peso = 71.40
+
+
+imc = peso / altura ** 2
+
+print (nome + " tem " + str(altura) +", pesa " + str(peso) + " e seu IMC é: " + str(imc))
